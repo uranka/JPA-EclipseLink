950abbed7383c61d81ec65394a3c9b09b6509eed
Jelena Gavanski <jelenagavanski@gmail.com>
2017-06-17 14:02:11 +0200

    SQL for jpastore database.

--- jpastore.sql
+++ jpastore.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 16, 2017 at 10:32 AM
+-- Generation Time: Jun 17, 2017 at 02:00 PM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -39,8 +39,8 @@ CREATE TABLE IF NOT EXISTS `customers` (
 --
 
 INSERT INTO `customers` (`customer_id`, `EMAIL`, `first_name`, `last_name`) VALUES
-(2, 'jeca@gmail.com', 'Jeca', 'Jecic'),
-(51, 'pedja@gmail.com', 'Pedja', 'Predic');
+(1, 'perap@gmail.com', 'Pera', 'Peric'),
+(3, 'mikaantic@gmail.com', 'Mika', 'Antic');
 
 -- --------------------------------------------------------
 
@@ -62,9 +62,53 @@ CREATE TABLE IF NOT EXISTS `invoices` (
 --
 
 INSERT INTO `invoices` (`invoice_number`, `invoice_date`, `is_processed`, `CUSTOMER_customer_id`) VALUES
-(3, '2017-05-26', 1, 2),
-(4, '2017-05-20', 1, 2),
-(52, '2016-12-25', 0, 51);
+(4, '2016-10-23', 1, 3),
+(7, '2016-12-10', 0, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lineitems`
+--
+
+CREATE TABLE IF NOT EXISTS `lineitems` (
+  `line_item_id` bigint(20) NOT NULL,
+  `QUANTITY` int(11) DEFAULT NULL,
+  `invoice_number` bigint(20) DEFAULT NULL,
+  `PRODUCT_product_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`line_item_id`),
+  KEY `FK_lineitems_PRODUCT_product_id` (`PRODUCT_product_id`),
+  KEY `FK_lineitems_invoice_number` (`invoice_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `lineitems`
+--
+
+INSERT INTO `lineitems` (`line_item_id`, `QUANTITY`, `invoice_number`, `PRODUCT_product_id`) VALUES
+(5, 100, 4, 2),
+(6, 3, 4, 2),
+(8, 15, 7, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE IF NOT EXISTS `products` (
+  `product_id` bigint(20) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `unit_price` decimal(8,2) DEFAULT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `name`, `unit_price`) VALUES
+(2, 'pegla', '990.99');
 
 -- --------------------------------------------------------
 
@@ -83,7 +127,7 @@ CREATE TABLE IF NOT EXISTS `sequence` (
 --
 
 INSERT INTO `sequence` (`SEQ_NAME`, `SEQ_COUNT`) VALUES
-('SEQ_GEN', '100');
+('SEQ_GEN', '50');
 
 --
 -- Constraints for dumped tables
@@ -94,6 +138,13 @@ INSERT INTO `sequence` (`SEQ_NAME`, `SEQ_COUNT`) VALUES
 --
 ALTER TABLE `invoices`
   ADD CONSTRAINT `FK_invoices_CUSTOMER_customer_id` FOREIGN KEY (`CUSTOMER_customer_id`) REFERENCES `customers` (`customer_id`);
+
+--
+-- Constraints for table `lineitems`
+--
+ALTER TABLE `lineitems`
+  ADD CONSTRAINT `FK_lineitems_invoice_number` FOREIGN KEY (`invoice_number`) REFERENCES `invoices` (`invoice_number`),
+  ADD CONSTRAINT `FK_lineitems_PRODUCT_product_id` FOREIGN KEY (`PRODUCT_product_id`) REFERENCES `products` (`product_id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
